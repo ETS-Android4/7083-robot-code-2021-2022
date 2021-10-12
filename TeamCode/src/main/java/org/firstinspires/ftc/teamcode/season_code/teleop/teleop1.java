e2ef78d2179fc1de4eb0b1b2e2e0eb96b37e974a
JBM180 <74985700+JBM180@users.noreply.github.com>
2021-10-11 20:39:04 -0400

    Added a bunch of teleop stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season_code/teleop/teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season_code/teleop/teleop1.java
@@ -5,43 +5,80 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.sun.tools.javac.tree.DCTree;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class teleop1 extends OpMode {
 
-    private DcMotor lbMotor;
-    private DcMotor rbMotor;
+    private DcMotor lrMotor;
+    private DcMotor rrMotor;
     private DcMotor lfMotor;
     private DcMotor rfMotor;
+    private Servo duck;
 
     @Override
     public void init() {
-        rbMotor = hardwareMap.get(DcMotor.class, "Right Back Motor");
+        rrMotor = hardwareMap.get(DcMotor.class, "Right Back Motor");
         lfMotor = hardwareMap.get(DcMotor.class, "Left Front Motor");
-        lbMotor = hardwareMap.get(DcMotor.class, "Left Back Motor");
+        lrMotor = hardwareMap.get(DcMotor.class, "Left Back Motor");
         rfMotor = hardwareMap.get(DcMotor.class, "Right Front Motor");
 
+        duck = hardwareMap.get(Servo.class, "Duck");
+        rrMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rfMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        double angle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x);
-        double mag = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) - Math.pow(gamepad1.left_stick_x, 2));
-        double turn = gamepad1.right_stick_x;
 
-        double power1 = Math.sin(angle - 0.25 * Math.PI) * mag;
-        double power2 = Math.sin(angle + 0.25 * Math.PI) * mag;
+        double forward = Math.pow(gamepad1.right_stick_y, 2);
+        if (gamepad1.right_stick_y < 0) {
+            forward *= -1;
+        }
+        double strafe = Math.pow(gamepad1.left_stick_x, 2);
+        if (gamepad1.left_stick_x > 0) {
+            strafe *= -1;
+        }
+        double turn = Math.pow(gamepad1.right_stick_x, 2);
+        if (gamepad1.right_stick_x > 0) {
+            turn *= -1;
+        }
 
-        rfMotor.setPower(power1 + turn);
-        lbMotor.setPower(power1 + turn);
+        if (gamepad1.a) {
+            duck.setPosition();
+        }
 
-        rbMotor.setPower(power2 + turn);
-        lfMotor.setPower(power2 + turn);
 
-        telemetry.addData("power1", power1);
-        telemetry.addData("power2", power2);
+        double angle = Math.atan2(forward, -gamepad1.left_stick_x) - Math.PI/4;
+        double mag = Math.hypot(forward, -gamepad1.left_stick_x);
+        //double turn = -gamepad1.right_stick_x;
+
+        double mod = 1;
+
+        if (gamepad1.right_bumper) {
+            mod = 0.5;
+        }
+
+        double lfPower = (mag * Math.cos(angle) + turn) * mod;
+        double rfPower = (mag * Math.sin(angle) - turn) * mod;
+        double lrPower = (mag * Math.sin(angle) + turn) * mod;
+        double rrPower = (mag * Math.cos(angle) - turn) * mod;
+
+
+
+        rfMotor.setPower(rfPower);
+        lrMotor.setPower(lrPower);
+
+        rrMotor.setPower(rrPower);
+        lfMotor.setPower(lfPower);
+
+        telemetry.addData("Left Front Power", lfPower);
+        telemetry.addData("Right Front Power", rfPower);
+        telemetry.addData("Right Back Power", rrPower);
+        telemetry.addData("Left Back Power", lfPower);
 
         telemetry.update();
     }

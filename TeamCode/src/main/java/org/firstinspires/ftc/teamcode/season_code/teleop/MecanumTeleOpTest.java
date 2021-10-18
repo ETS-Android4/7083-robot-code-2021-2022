2b69a0562dc51a537a5883d10c99faa3d0412309
matursam <mtursam@gmail.com>
2021-10-18 18:50:07 -0400

    Renamed mecanum teleop opmode to more descriptive name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season_code/teleop/MecanumTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season_code/teleop/MecanumTeleOpTest.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import static java.lang.Thread.sleep;
 
 @TeleOp
-public class teleop1 extends OpMode {
+public class MecanumTeleOpTest extends OpMode {
     private DcMotor lrMotor;
     private DcMotor rrMotor;
     private DcMotor lfMotor;
